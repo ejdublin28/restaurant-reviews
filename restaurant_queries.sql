4fab71b3ba759f5c6bf26bec45c24937e59727fd
Emma Dublin <emmadublin@Emmas-MacBook-Pro-8.local>
2022-03-24 21:21:07 -0500

    adding restaurant and inspection tables to schema

--- restaurant_queries.sql
+++ restaurant_queries.sql
@@ -1,11 +1,44 @@
+DROP TABLE IF EXISTS inspections;
+
+CREATE TABLE inspections (
+	inspection_no INT PRIMARY KEY,
+	dba_name VARCHAR NOT NULL,
+	license_no DEC NOT NULL,
+	facility_type VARCHAR,
+	risk VARCHAR,
+	address VARCHAR,
+	city VARCHAR,
+	state VARCHAR,
+	zip VARCHAR,
+	inspection_date DATE NOT NULL,
+	inspection_type VARCHAR,
+	results VARCHAR,
+	violations VARCHAR,
+	latitude DEC,
+	longitude DEC,
+	location VARCHAR
+
+);
+
+DROP TABLE IF EXISTS google_results;
+
+CREATE TABLE google_results (
+	id SERIAL PRIMARY KEY,
+	aka_name VARCHAR,
+	vicinity VARCHAR,
+	rating DEC,
+	business_status VARCHAR
+	
+);
+
 DROP TABLE IF EXISTS merged_restaurants;
 
 -- create table to import data
 CREATE TABLE merged_restaurants (
 	extra INT,
-	inspection_no INT,
-	dba_name VARCHAR,
-	aka_name VARCHAR,
+	inspection_no INT NOT NULL,
+	dba_name VARCHAR NOT NULL,
+	aka_name VARCHAR NOT NULL,
 	license_no DEC,
 	facility_type VARCHAR,
 	risk VARCHAR,
@@ -22,6 +55,7 @@ CREATE TABLE merged_restaurants (
 	location VARCHAR,
 	rating DEC, 
 	business_status VARCHAR
+	FOREIGN KEY (inspection_no) REFERENCES inspections(inspection_no)
 );
 
 -- import table from merged_restaurants.csv
